8da8157ba9fae2f169ec1c62c5642d68ca32469f
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-05-19 12:14:10 +0800

    disaster game view

--- src/com/company/Sprite/Door.java
+++ src/com/company/Sprite/Door.java
@@ -1,4 +1,15 @@
 package com.company.Sprite;
 
-public class Door {
+import javax.swing.*;
+
+public class Door extends Sprite {
+    public Door(int x, int y) {
+        setPosition(x, y);
+        img = new ImageIcon("door.png");
+    }
+
+    @Override
+    public String overlap(int x, int y) {
+        return null;
+    }
 }
--- src/com/company/Sprite/Sprite.java
+++ src/com/company/Sprite/Sprite.java
@@ -1,11 +1,11 @@
 package com.company.Sprite;
 
 //import com.sun.tools.javac.Main;
+
 import com.company.Main;
 
 import javax.swing.*;
 import java.awt.*;
-
 
 
 public abstract class Sprite {
@@ -16,9 +16,29 @@ public abstract class Sprite {
     public void draw(Graphics g) {
         img.paintIcon(null, g, absolutePosition.x, absolutePosition.y);
     }
-    public void setPosition(int x ,int y){
-        relativePosition = new Point(x,y);
-        absolutePosition = new Point((x-1)* Main.CELL,(y-1)*Main.CELL);
+
+    public void setPosition(Point p) {
+        setPosition(p.x, p.y);
     }
+
+    public void setPosition(int x, int y) {
+        relativePosition = new Point(x, y);
+        absolutePosition = new Point((x - 1) * Main.CELL, (y - 1) * Main.CELL);
+    }
+
+    public void setNullPositioin() {
+        relativePosition = null;
+        absolutePosition = null;
+    }
+
+    public Point getRelativePosition() {
+        if (relativePosition == null) {
+            return null;
+        } else {
+            return new Point(relativePosition);
+        }
+    }
+
+    public abstract String overlap(int x, int y);
 
 }
